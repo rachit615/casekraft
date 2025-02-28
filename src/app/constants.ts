742a59ec0eb1a7730935a317b1235cd372940439
Rachit Gupta <70112715+rachit615@users.noreply.github.com>
2025-02-28 16:04:55 +0530

    Add files via upload

---
+++ src/app/constants.ts
@@ -0,0 +1,26 @@
+export const users = [
+  "/users/user-1.png",
+  "/users/user-2.png",
+  "/users/user-3.png",
+  "/users/user-4.jpg",
+  "/users/user-5.jpg",
+];
+export const features = [
+  "High-quality, durable material",
+  "5 year print guarantee",
+  "Modern iPhone models supported",
+];
+export const reviews = [
+  {
+    name: "Jonathan",
+    imgSrc: "/users/user-1.png",
+    review:
+      "The case feels durable and I even got a compliment on the design. The image is super clear, unlike other cases that fade over time. Love it!",
+  },
+  {
+    name: "Josh",
+    imgSrc: "/users/user-4.jpg",
+    review:
+      "I keep my phone with my keys, but this case still looks brand new after half a year. I dig it!",
+  },
+];
